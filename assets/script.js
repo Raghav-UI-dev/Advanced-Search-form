3bfdfa5054d29beeb8e14f2db6d257a8e0540b3b
Raghav-UI-dev <raghav.scode@gmail.com>
2024-08-26 16:39:58 +0530

    Assets

---
+++ assets/script.js
@@ -0,0 +1,134 @@
+
+
+
+
+        document.getElementById('showButton').addEventListener('click', function() {
+            const hiddenField = document.getElementById('hiddenField');
+
+            // Toggle visibility
+            if (hiddenField.style.display === 'none' || hiddenField.style.display === '') {
+                hiddenField.style.display = 'block'; // Show the input field
+            } else {
+                hiddenField.style.display = 'none'; // Hide the input field (if needed)
+            }
+        });
+
+        
+    
+    
+        document.getElementById('showButton1').addEventListener('click', function() {
+            const hiddenFieldO = document.getElementById('hiddenFieldO');
+
+            // Toggle visibility
+            if (hiddenFieldO.style.display === 'none' || hiddenFieldO.style.display === '') {
+                hiddenFieldO.style.display = 'flex'; // Show the input field
+            } else {
+                hiddenFieldO.style.display = 'none'; // Hide the input field (if needed)
+            }
+        });
+
+        document.getElementById('showButton2').addEventListener('click', function() {
+            const hiddenFieldO = document.getElementById('hiddenFieldC');
+
+            // Toggle visibility
+            if (hiddenFieldO.style.display === 'none' || hiddenFieldO.style.display === '') {
+                hiddenFieldO.style.display = 'block'; // Show the input field
+            } else {
+                hiddenFieldO.style.display = 'none'; // Hide the input field (if needed)
+            }
+        });
+
+        
+
+        document.querySelector('.tagInput').addEventListener('keydown', function(event) {
+            if (event.key === ',') {
+                event.preventDefault(); // Prevent the form submission or other default actions
+                const inputText = this.value.trim(); // Get the input value and trim whitespace
+
+                if (inputText) {
+                    const tags = inputText.split(/\s*,\s*|\s+/); // Split by comma or space
+
+                    tags.forEach(tag => {
+                        const tagElement = document.createElement('span');
+                        tagElement.className = 'tag';
+                        tagElement.textContent = tag;
+                        
+                        const removeButton = document.createElement('span');
+                        removeButton.className = 'remove-tag';
+                        removeButton.textContent = '×';
+                        removeButton.onclick = function() {
+                            this.parentElement.remove();
+                        };
+                        tagElement.appendChild(removeButton);
+                        document.getElementById('tagsContainer').appendChild(tagElement);
+                    });
+                }
+            }
+        });
+
+document.querySelector('.tagInputn').addEventListener('keydown', function(event) {
+            if (event.key === ',') {
+                event.preventDefault(); // Prevent the form submission or other default actions
+                const inputText = this.value.trim(); // Get the input value and trim whitespace
+
+                if (inputText) {
+                    const tags = inputText.split(/\s*,\s*|\s+/); // Split by comma or space
+
+                    tags.forEach(tag => {
+                        const tagElement = document.createElement('span');
+                        tagElement.className = 'tag';
+                        tagElement.textContent = tag;
+                        
+                        const removeButton = document.createElement('span');
+                        removeButton.className = 'remove-tag';
+                        removeButton.textContent = '×';
+                        removeButton.onclick = function() {
+                            this.parentElement.remove();
+                        };
+                        tagElement.appendChild(removeButton);
+                        document.getElementById('tagsContainern').appendChild(tagElement);
+                        
+                    });
+                }
+            }
+        });
+
+
+        document.querySelector('.tagInputnn').addEventListener('keydown', function(event) {
+            if (event.key === ',') {
+                event.preventDefault(); // Prevent the form submission or other default actions
+                const inputText = this.value.trim(); // Get the input value and trim whitespace
+
+                if (inputText) {
+                    const tags = inputText.split(/\s*,\s*|\s+/); // Split by comma or space
+
+                    tags.forEach(tag => {
+                        const tagElement = document.createElement('span');
+                        tagElement.className = 'tag';
+                        tagElement.textContent = tag;
+                        
+                        const removeButton = document.createElement('span');
+                        removeButton.className = 'remove-tag';
+                        removeButton.textContent = '×';
+                        removeButton.onclick = function() {
+                            this.parentElement.remove();
+                        };
+                        tagElement.appendChild(removeButton);
+                        document.getElementById('tagsContainernn').appendChild(tagElement);
+                        
+                    });
+                }
+            }
+        });
+    
+
+
+        const links = document.querySelectorAll('.toggle-link');
+
+        links.forEach(link => {
+            link.addEventListener('click', function (event) {
+
+                links.forEach(l => l.classList.remove('active'));
+                this.classList.add('active');
+            });
+        });
